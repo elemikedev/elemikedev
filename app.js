bbd1000d64082cde5a5a32e3504823fc6c7b2e5e
elemikeDev <miketorresdj1@hotmail.com>
2022-10-07 11:57:26 -0500

    eliminando numeros negativos y poniendo limite en los inputs

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+class CampoNumerico {
+
+    constructor(selector) {
+    this.nodo = document.querySelector(selector);
+    this.valor = '';
+    
+    this.empezarAEscucharEventos();
+    }
+    
+    empezarAEscucharEventos() {
+    this.nodo.addEventListener('keydown', function(evento) {
+        const teclaPresionada = evento.key;
+        const teclaPresionadaEsUnNumero =
+        Number.isInteger(parseInt(teclaPresionada));
+
+        const sePresionoUnaTeclaNoAdmitida = 
+        teclaPresionada != 'ArrowDown' &&
+        teclaPresionada != 'ArrowUp' &&
+        teclaPresionada != 'ArrowLeft' &&
+        teclaPresionada != 'ArrowRight' &&
+        teclaPresionada != 'Backspace' &&
+        teclaPresionada != 'Delete' &&
+        teclaPresionada != 'Enter' &&
+        !teclaPresionadaEsUnNumero;
+        const comienzaPorCero =
+        this.nodo.value.length === 0 &&
+        teclaPresionada == 0;
+
+        if (sePresionoUnaTeclaNoAdmitida || comienzaPorCero) {
+        evento.preventDefault();
+        } else if (teclaPresionadaEsUnNumero) {
+        this.valor += String(teclaPresionada);
+        }
+
+    }.bind(this));
+
+    this.nodo.addEventListener('input', function(evento) {
+        const cumpleFormatoEsperado = new RegExp(/^[0-9]+/).test(this.nodo.value);
+
+        if (!cumpleFormatoEsperado) {
+        this.nodo.value = this.valor;
+        } else {
+        this.valor = this.nodo.value;
+        }
+    }.bind(this));
+    }
+
+}
+new CampoNumerico('#numero');
+
+
 let sidebar = document.querySelector(".sidebar");
     let sidebarBtn = document.querySelector(".sidebarBtn");
     sidebarBtn.onclick = function() {
@@ -7,3 +58,4 @@ let sidebar = document.querySelector(".sidebar");
     }else
     sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
+
